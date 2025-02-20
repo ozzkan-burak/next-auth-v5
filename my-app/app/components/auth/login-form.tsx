f0f17d94dc820fcd8d68d2ef1e8d50f1e8cea08e
ozzkan-burak <brk.ozzkan@gmail.com>
2025-02-20 21:29:31 +0300

    episode13

--- my-app/app/components/auth/login-form.tsx
+++ my-app/app/components/auth/login-form.tsx
@@ -1,12 +1,26 @@
-'use-client'
+'use client'
 
+import * as z from 'zod'
 import { CardWrapper } from './card-wrapper'
 import { zodResolver } from '@hookform/resolvers/zod'
-import * as z from 'zod'
 import { useForm } from 'react-hook-form'
 import { LoginSchema } from '@/schema'
+import { Form, FormControl, FormField, FormItem, FormLabel } from '../ui/form'
+import { Input } from '../ui/input'
 
 export const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  const onSubmit = (data: z.infer<typeof LoginSchema>) => {
+    console.log(data)
+  }
+
   return (
     <>
       <CardWrapper
@@ -15,7 +29,24 @@ export const LoginForm = () => {
         backButtonLabel="Dont have an Account"
         showSocial
       >
-        <div>Login Form</div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <div>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }: any) => (
+                  <FormItem>
+                    <FormLabel htmlFor={field.name}>Email</FormLabel>
+                    <FormControl>
+                      <Input type="email" {...field} placeholder="email" />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+          </form>
+        </Form>
       </CardWrapper>
     </>
   )
