087e47be7a2b12adb4088294eeccd3af03c9fe7d
ozzkan-burak <brk.ozzkan@gmail.com>
2025-03-28 23:50:15 +0300

    episode30

--- my-app/auth.ts
+++ my-app/auth.ts
@@ -4,7 +4,19 @@ import { PrismaClient } from '@prisma/client'
 import { db } from './lib/db'
 import authconfig from './auth.config'
 
-export const { auth, handlers, signIn, signOut } = NextAuth({
+export const {
+  auth,
+  handlers: { GET, POST },
+  signIn,
+  signOut,
+} = NextAuth({
+  callbacks: {
+    async jwt({ token }) {
+      if (!token.sub) return token
+      console.log('jwt', token)
+      return token
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   ...authconfig,
