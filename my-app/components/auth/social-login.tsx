6582d739069ae9069234670d7f7ad0dcd1807687
ozzkan-burak <brk.ozzkan@gmail.com>
2025-03-30 01:32:30 +0300

    feat:episode34

--- my-app/components/auth/social-login.tsx
+++ my-app/components/auth/social-login.tsx
@@ -1,7 +1,15 @@
 import { Button } from '../ui/button'
 import { FaGoogle, FaGithub } from 'react-icons/fa'
+import { signIn } from 'next-auth/react'
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes'
 
 export const SocialLogin = () => {
+  const handleSocialLogin = (provider: 'github') => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    })
+  }
+
   return (
     <div className="flex items-center w-full gap-9 pr-6 pl-6">
       <Button
@@ -16,7 +24,7 @@ export const SocialLogin = () => {
         size="lg"
         className="w-full"
         variant="github"
-        //onClick={() => console.log('Google')}
+        onClick={() => handleSocialLogin('github')}
       >
         <FaGithub size={22} className="text-xl" />
       </Button>
