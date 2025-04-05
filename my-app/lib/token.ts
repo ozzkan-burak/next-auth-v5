562870d4e9a88573e14ab59764d671f38cba6a25
ozzkan-burak <brk.ozzkan@gmail.com>
2025-04-06 01:43:30 +0300

    feat: episode38

---
+++ my-app/lib/token.ts
@@ -0,0 +1,23 @@
+import { db } from '@/lib/db'
+import { getVerificationEmail } from '@/data/verification-token'
+import { v4 as uuidv4 } from 'uuid'
+
+export const generateVerificationToken = async (email: string) => {
+  const token = uuidv4()
+  const expires = new Date(Date.now() + 60 * 60 * 1000) // 1 hour expiration
+  const exitingToken = await getVerificationEmail(email)
+  if (exitingToken) {
+    await db.verificationToken.delete({
+      where: { token: exitingToken.verificationToken.token },
+    })
+  }
+
+  const verificationToken = await db.verificationToken.create({
+    data: {
+      token,
+      email,
+      expires,
+    },
+  })
+  return verificationToken
+}
